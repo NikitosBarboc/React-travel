793eb65c3da7e8e8762be232ec06160778754b25
Nik-portfolio <kravtcovmikita@gmail.com>
2022-09-12 13:49:39 +0300

    feat: finish second lesson (jsx lesson)

--- 02_jsx/my-app/src/index.tsx
+++ 02_jsx/my-app/src/index.tsx
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 const user = {
-  name: "Vacya",
-  age: 44
+  name: 'Vacya',
+  age: 44,
+  city: 'Kiev',
+  profileLink: 'https://www.instagram.com/fan.patron/',
+};
+
+const profileLink = <a href={user.profileLink}>Profile</a>;
+
+function getUserInfo(): string {
+  return `${user.name} is ${user.age} years old `;
 }
 root.render(
   <React.StrictMode>
-    
-  </React.StrictMode>
+    {`${getUserInfo()} and he and lives in  ${user.city}`}
+    {profileLink}
+    {}
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
