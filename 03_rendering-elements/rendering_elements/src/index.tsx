88e8e2200fe729a177bcb0737f2cc13e0e1a13f4
Nik-portfolio <kravtcovmikita@gmail.com>
2022-09-12 15:55:30 +0300

    feat: finish lesson 03_rendering-elements

--- 03_rendering-elements/rendering_elements/src/index.tsx
+++ 03_rendering-elements/rendering_elements/src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.querySelector('#root') as HTMLDivElement,
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const element = <h1>Hello, word!</h1>;
+
+// root.render(
+//   <React.StrictMode>
+//     {element}
+//     {element}
+//   </React.StrictMode>,
+// );
+function timer() {
+  const now = new Date();
+  const timeZone = now.toString().slice(now.toString().indexOf('('));
+  const element = (
+    <div>
+      <h1>Timer</h1>
+      <h2>
+        {`It is ${now.toLocaleTimeString()} in ${timeZone}`}
+      </h2>
+    </div>
+  );
+  root.render(element);
+}
+setInterval(timer, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
